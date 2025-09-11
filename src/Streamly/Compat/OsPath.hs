64ad848c20314c397a589ac173774cbb2cb382ac
Harendra Kumar <harendra@composewell.com>
2025-09-11 17:38:54 +0530

    Add unsafeToPath, make APIs portable

--- src/Streamly/Compat/OsPath.hs
+++ src/Streamly/Compat/OsPath.hs
@@ -5,14 +5,13 @@
 {-# LANGUAGE PackageImports #-}
 
 module Streamly.Compat.OsPath
-    ( toByteArray
-    , fromByteArray
+    ( unsafeToPath
     , toPath
     , fromPath
     )
 where
 
-import Data.Word (Word8)
+import Control.Monad.Catch (MonadThrow)
 #if MIN_VERSION_filepath(1,5,0)
 import System.OsString.Internal.Types (OsString(..), PosixString(..))
 import "os-string" System.OsString.Data.ByteString.Short (ShortByteString(..))
@@ -22,7 +21,7 @@ import "filepath" System.OsPath.Data.ByteString.Short (ShortByteString(..))
 #endif
 import Streamly.Internal.Data.MutByteArray (MutByteArray(..))
 import Streamly.Internal.Data.Array (Array(..))
-import Streamly.Internal.FileSystem.Path (Path)
+import Streamly.FileSystem.Path (Path, OsWord)
 import System.OsPath (OsPath)
 import GHC.Exts (sizeofByteArray#, Int(..), unsafeCoerce#)
 import System.IO.Unsafe (unsafePerformIO)
@@ -30,26 +29,37 @@ import System.IO.Unsafe (unsafePerformIO)
 import qualified Streamly.Internal.Data.MutByteArray as MBA
 import qualified Streamly.Internal.FileSystem.Path as Path
 
-toByteArray :: OsPath -> Array Word8
-toByteArray p =
+toOsWordArray :: OsPath -> Array OsWord
+toOsWordArray p =
     let !(SBS barr#) = getPosixString $ getOsString p
         mbarr = MutByteArray (unsafeCoerce# barr#)
-      in Array mbarr 0 (I# (sizeofByteArray# barr#)) :: Array Word8
+      in Array mbarr 0 (I# (sizeofByteArray# barr#)) :: Array OsWord
 
-fromByteArray :: Array Word8 -> OsPath
-fromByteArray (Array mba@(MutByteArray mbarr#) start end) =
+fromOsWordArray :: Array OsWord -> OsPath
+fromOsWordArray (Array mba@(MutByteArray mbarr#) start end) =
     let sz = I# (sizeofByteArray# (unsafeCoerce# mbarr#))
      in if start == 0 && end == sz
         -- freeze it instead of coercing? What if mba has been coerced from a
-        -- frozen byetarray e.g. in toByteArray above?
+        -- frozen byetarray e.g. in toOsWordArray above?
         then OsString $ PosixString (SBS (unsafeCoerce# mbarr#))
         else let !(MutByteArray arr#) =
                     unsafePerformIO
                         $ MBA.unsafeCloneSlice start (end - start) mba
              in OsString $ PosixString (SBS (unsafeCoerce# arr#))
 
-toPath :: OsPath -> Path
-toPath = Path.unsafeFromArray . toByteArray
+-- | Adapt an 'OsPath' type to the streamly 'Path' type at zero cost.
+--
+-- Unsafe: The user is responsible to make sure that the 'OsPath' is valid as
+-- per 'Path.validatePath'.
+--
+unsafeToPath :: OsPath -> Path
+unsafeToPath = Path.unsafeFromArray . toOsWordArray
 
+-- | Adapt an 'OsPath' type to the streamly 'Path' type at zero cost.
+--
+toPath :: MonadThrow m => OsPath -> m Path
+toPath = Path.fromArray . toOsWordArray
+
+-- | Adapt a Streamly 'Path' type to 'OsPath' type at zero cost.
 fromPath :: Path -> OsPath
-fromPath = fromByteArray . Path.toArray
+fromPath = fromOsWordArray . Path.toArray
