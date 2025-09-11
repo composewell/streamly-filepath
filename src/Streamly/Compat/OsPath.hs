0de3469b05440a6c783008cff450cfcdb4be0c3a
Harendra Kumar <harendra@composewell.com>
2025-09-11 22:14:41 +0530

    Fix Windows build

--- src/Streamly/Compat/OsPath.hs
+++ src/Streamly/Compat/OsPath.hs
@@ -4,6 +4,8 @@
 {-# LANGUAGE MagicHash #-}
 {-# LANGUAGE PackageImports #-}
 
+{-# OPTIONS_GHC -Wno-deprecations #-}
+
 module Streamly.Compat.OsPath
     ( unsafeToPath
     , toPath
@@ -13,10 +15,20 @@ where
 
 import Control.Monad.Catch (MonadThrow)
 #if MIN_VERSION_filepath(1,5,0)
-import System.OsString.Internal.Types (OsString(..), PosixString(..))
+import System.OsString.Internal.Types (OsString(..))
+#if defined(mingw32_HOST_OS)
+import System.OsString.Internal.Types (WindowsString(..))
+#else
+import System.OsString.Internal.Types (PosixString(..))
+#endif
 import "os-string" System.OsString.Data.ByteString.Short (ShortByteString(..))
 #else
-import "filepath" System.OsString.Internal.Types (OsString(..), PosixString(..))
+import "filepath" System.OsString.Internal.Types (OsString(..))
+#if defined(mingw32_HOST_OS)
+import "filepath" System.OsString.Internal.Types (WindowsString(..))
+#else
+import "filepath" System.OsString.Internal.Types (PosixString(..))
+#endif
 import "filepath" System.OsPath.Data.ByteString.Short (ShortByteString(..))
 #endif
 import Streamly.Internal.Data.MutByteArray (MutByteArray(..))
@@ -29,23 +41,41 @@ import System.IO.Unsafe (unsafePerformIO)
 import qualified Streamly.Internal.Data.MutByteArray as MBA
 import qualified Streamly.Internal.FileSystem.Path as Path
 
+#if defined(mingw32_HOST_OS)
+getString :: WindowsString -> ShortByteString
+getString = getWindowsString
+#else
+getString :: PosixString -> ShortByteString
+getString = getPosixString
+#endif
+
+#if defined(mingw32_HOST_OS)
+mkString :: ShortByteString -> WindowsString
+mkString = WindowsString
+#else
+mkString :: ShortByteString -> PosixString
+mkString = PosixString
+#endif
+
+{-# INLINE toOsWordArray #-}
 toOsWordArray :: OsPath -> Array OsWord
 toOsWordArray p =
-    let !(SBS barr#) = getPosixString $ getOsString p
+    let !(SBS barr#) = getString $ getOsString p
         mbarr = MutByteArray (unsafeCoerce# barr#)
       in Array mbarr 0 (I# (sizeofByteArray# barr#)) :: Array OsWord
 
+{-# INLINE fromOsWordArray #-}
 fromOsWordArray :: Array OsWord -> OsPath
 fromOsWordArray (Array mba@(MutByteArray mbarr#) start end) =
     let sz = I# (sizeofByteArray# (unsafeCoerce# mbarr#))
      in if start == 0 && end == sz
         -- freeze it instead of coercing? What if mba has been coerced from a
         -- frozen byetarray e.g. in toOsWordArray above?
-        then OsString $ PosixString (SBS (unsafeCoerce# mbarr#))
+        then OsString $ mkString (SBS (unsafeCoerce# mbarr#))
         else let !(MutByteArray arr#) =
                     unsafePerformIO
                         $ MBA.unsafeCloneSlice start (end - start) mba
-             in OsString $ PosixString (SBS (unsafeCoerce# arr#))
+             in OsString $ mkString (SBS (unsafeCoerce# arr#))
 
 -- | Adapt an 'OsPath' type to the streamly 'Path' type at zero cost.
 --
